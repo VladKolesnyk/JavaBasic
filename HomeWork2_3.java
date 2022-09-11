ee9d679cb9f76c49dabf810a96d98505160be59a
VladKolesnyk <kolesnik.vladyslav@gmail.com>
2022-09-11 15:02:25 +0200

    Home

--- HomeWork2_3.java
+++ HomeWork2_3.java
@@ -41,33 +41,4 @@ class HomeWork2_1 {
         System.out.println("cannot be divided by 0");
         */
 
-    // Task 4
-        public static void main(String[] args) {
-        int d = 2;
-        switch (d) {
-            case 1:
-                System.out.println("It's Monday");
-                break;
-            case 2:
-                System.out.println("It's Tuesday");
-                break;
-            case 3:
-                System.out.println("It's Wednesday");
-                break;
-            case 4:
-                System.out.println("It's Thursday");
-                break;
-            case 5:
-                System.out.println("It's Friday");
-                break;
-            case 6:
-                System.out.println("It's Saturday");
-                break;
-            case 7:
-                System.out.println("It's Sunday");
-                break;
-            default:
-                System.out.println("Error Day number!");
-                break;
-        }
     }
